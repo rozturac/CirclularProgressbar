a99abc8dacb7cad1fa230c67274bc3e24e49ac01
Rıdvan <46276980+rozturac@users.noreply.github.com>
2019-07-28 18:00:09 +0300

    Update CircularProgressBar.cs

--- CircularProgressBar.cs
+++ CircularProgressBar.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Windows.Forms;
 
-namespace RSoft.GsmStock.WinForm.CusomComponents
+namespace RSoft.Componenets.CusomComponents
 {
     [ToolboxItem(true)]
     public class CircularProgressBar : Label
